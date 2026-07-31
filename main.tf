28225835868bdd8ad1635338befb9bbe76c948f9
Tim Reynolds <tim.rey1@googlemail.com>
2026-07-31 14:58:40 +0000

    Update to EC2 type

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-central-2"
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
+  owners      = ["099720109477"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
-
-  owners = ["099720109477"] # Canonical
 }
 #adding comment to test2
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   count         = 2
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "learn-terraform"
@@ -25,4 +23,3 @@ resource "aws_instance" "app_server" {
 
 resource "aws_s3_bucket" "s3" {
   bucket = "tr-terraform-test159"
-}
